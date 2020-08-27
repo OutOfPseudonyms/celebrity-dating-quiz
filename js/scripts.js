d90f0ec1fef69c4144ee38cbb633b5722841707a
Patrick Swaney <prswaney@gmail.com>
2020-08-27 13:28:35 -0700

    create basic html, most javascript.

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $("form#celebrityElegibility").submit(function(event) {
+    const age = parseInt($("input#age").val());
+    const band = $("input#band").val();
+    const allergies = $("input#allergies").val();
+
+    if age 
+    }
+  }
+})
