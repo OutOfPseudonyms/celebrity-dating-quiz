b55c8dbeca35e2f0396245a7c76c903e0c226093
Patrick Swaney <prswaney@gmail.com>
2020-08-28 14:15:59 -0700

    complete everything but validation.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,18 @@
-$(document).ready(function() {
-  $("form#celebrityElegibility").submit(function(event) {
+$(document).ready(function () {
+  $("form#celebrityElegibility").submit(function (event) {
+    event.preventDefault();
     const age = parseInt($("input#age").val());
     const band = $("input#band").val();
     const allergies = $("input#allergies").val();
 
-    if age 
+    $('#results').show();
+
+    if (age >= 25 && age <= 45) {
+      $('#celebrity').text("Ben Diskin");
+    } else if (age >= 46 && age <= 70) {
+      $('#celebrity').text("Steven Blum");
+    } else if (age >= 71) {
+      $('#celebrity').text("Elvis Presley");
     }
-  }
-})
+  })
+});
